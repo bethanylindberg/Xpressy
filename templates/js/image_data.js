f6984867cd879f5d019fd8688bff87511185e7e1
Kavya <skavya90@gmail.com>
2019-07-25 18:17:54 -0500

    Updated choose image feature

--- templates/js/image_data.js
+++ templates/js/image_data.js
@@ -1,5 +1,6 @@
 //parsing data 
 var tDataran = JSON.parse(image_data_var);
+console.log(tDataran.length);
 // shuffles image selection for carousel
 var tData = shuffle(tDataran);
 //console.log('Random Data is',tData)
@@ -53,19 +54,18 @@ function shuffle(imagelist) {
 
 //Refers to image related data table
 var tableContent = document.querySelector("#tabledata");
-//Component MutationObserver observes any changes in attribute values and gets them to mutationRecords
+//Create Mutation Observer to detect changes
 let observer = new MutationObserver(mutationRecords => {
-  //console.log(mutationRecords); // console.log(the changes)
-  var imageContent = document.getElementsByClassName("rondell-item rondell-item-focused");
-
-  if (imageContent[0] !== undefined) {
-
-    var imgid = imageContent[0].id;
-
-    var data = JSON.parse(image_data_var);
-    for (i = 0; i < data.length; i++) {
-      if (data[i]["Image_ID"] == imgid) {
-        tableContent.innerHTML = "";
+  //Check if data is changed  or only style is changed
+  if (mutationRecords[0].addedNodes.length > 0 || mutationRecords[0].removedNodes.length > 0) {
+    // if data is changed then it indicates that new image was added and populate data for single image
+    var imageContent = document.getElementById('singleImage')
+    if (imageContent !== null) {
+    var imgid = imageContent.title;
+    //Loops through imagelist
+    for (i = 0; i < tDataran.length; i++) {
+      if (tDataran[i]["Image_ID"] == imgid) {
+        tableContent.innerHTML = ""; 
         trhead = document.createElement("tr");
         thead1 = document.createElement("th");
         thead2 = document.createElement("th");
@@ -76,15 +76,16 @@ let observer = new MutationObserver(mutationRecords => {
         trhead.appendChild(thead1);
         trhead.appendChild(thead2);
         tableContent.appendChild(trhead);
-
-        for (var key in data[i]) {
+        //if i matches with imgid populate table as below
+        for (var key in tDataran[i]) {
+          //populate data for all keys except "Image_ID" and "Label"
           if (String(key) == "Image_ID" || String(key) == "label") {
           } else {
             row = document.createElement("tr");
             cell1 = document.createElement("td");
             cell2 = document.createElement("td");
             textnode1 = document.createTextNode(key);
-            textnode2 = document.createTextNode(data[i][key]);
+            textnode2 = document.createTextNode(tDataran[i][key]);
             cell1.appendChild(textnode1);
             cell2.appendChild(textnode2);
             row.appendChild(cell1);
@@ -95,8 +96,55 @@ let observer = new MutationObserver(mutationRecords => {
         break;
       }
     }
+  } 
+  }
+  else {
+    // if styale is changed then it indicates caurousel mode is on , populate data for focused image
+    var imageContent = document.getElementsByClassName("rondell-item rondell-item-focused");
+    //console.log('image content is', imageContent)
+    if (imageContent[0] !== undefined) {
+
+      var imgid = imageContent[0].id;
+      //Loops through Imagelist
+      for (i = 0; i < tDataran.length; i++) {
+        if (tDataran[i]["Image_ID"] == imgid) {
+
+          tableContent.innerHTML = "";
+          trhead = document.createElement("tr");
+          thead1 = document.createElement("th");
+          thead2 = document.createElement("th");
+          text1 = document.createTextNode("Emotion");
+          text2 = document.createTextNode("Prediction (%)");
+          thead1.appendChild(text1);
+          thead2.appendChild(text2);
+          trhead.appendChild(thead1);
+          trhead.appendChild(thead2);
+          tableContent.appendChild(trhead);
+
+          //if i matches with imgid populate table as below
+          for (var key in tDataran[i]) {
+            //populate data for all keys except "Image_ID" and "Label"
+            if (String(key) == "Image_ID" || String(key) == "label") {
+            } else {
+              row = document.createElement("tr");
+              cell1 = document.createElement("td");
+              cell2 = document.createElement("td");
+              textnode1 = document.createTextNode(key);
+              textnode2 = document.createTextNode(tDataran[i][key]);
+              cell1.appendChild(textnode1);
+              cell2.appendChild(textnode2);
+              row.appendChild(cell1);
+              row.appendChild(cell2);
+              tableContent.appendChild(row);
+            }
+          }
+          break;
+        }
+      }
+    }
   }
 });
+
 observer.observe(rondellCarousel, {
   // observe everything except attributes
   childList: true,
@@ -104,3 +152,44 @@ observer.observe(rondellCarousel, {
   attributes: true,
   attributeFilter: ['style']
 });
+
+//Gets image_id  of the selected image
+function readURL(input) {
+  fileName = document.getElementById('fileInputId').files[0].name
+
+  var $imageContent = document.querySelector("#rondellCarousel");
+  //selects the div container inside rondellCarousel
+  var $imageContentdiv = document.getElementsByClassName('rondell-container');
+  //Clears the current image being displayed
+  while ($imageContent.firstChild) {
+    $imageContent.removeChild($imageContent.firstChild);
+  }
+  //Creates a and div tags to populate the container with selected image
+  var aTag = document.createElement('a');
+  var divTag = document.createElement('div');
+  divTag.setAttribute('class', "rondell-container rondell-theme-light rondell-instance-1")
+  divTag.setAttribute('style', "width: 680px; height: 300px;")
+
+  //populates data for selected image
+  var imageVal = 'Stock_Images/Gallery/' + fileName;
+  aTag.setAttribute('href', imageVal)
+  aTag.setAttribute('target', "_blank")
+  aTag.setAttribute('rel', "rondell_1")
+  aTag.setAttribute('title', fileName)
+  aTag.setAttribute('class', "rondell-item rondell-item-focused")
+  aTag.setAttribute('style', "opacity: 1; width: 300px; height: 188px; left: 190px; top: 66px; z-index: 1001; display: block;")
+
+
+  var imgTag = document.createElement('img');
+  imgTag.setAttribute('src', imageVal)
+  imgTag.setAttribute('class', "rondell-item-image rondell-item-resizeable")
+
+  var h5Tag = document.createElement('h5');
+  //Creates a unque id(singleImage) specific to new image chosen
+  aTag.setAttribute('id', 'singleImage');
+  h5Tag.innerHTML = imageVal;
+  aTag.appendChild(imgTag);
+  aTag.appendChild(h5Tag);
+  divTag.appendChild(aTag);
+  $imageContent.appendChild(divTag);
+}
